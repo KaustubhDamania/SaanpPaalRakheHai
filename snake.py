4264f8f57bf51fa44f32f28abb49e8b4915916d3
Kaustubh Damania <33577657+KaustubhDamania@users.noreply.github.com>
2018-10-30 16:55:11 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -19,7 +19,7 @@ w_height = 600
 white=(255,255,255)
 black=(0,0,0)
 red=(255,0,0)
-FPS=30
+FPS=100
 blockSize=10
 init_x = math.floor(144/10)*10 #random.randint(0,w_height)
 init_y = math.floor(133/10)*10 #random.randint(0,w_width)
@@ -27,14 +27,15 @@ snakeList=[]
 snakeLength=10
 snakeHead = [60,60] #snakehead starting coordinate, update this
 snakeList.append(snakeHead)
-snakeList.append([60,50])
-snakeList.append([60,40])
-snakeList.append([60,30])
+#snakeList.append([60,50])
+#snakeList.append([60,40])
+#snakeList.append([60,30])
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((w_width,w_height))
-check=not True
+#check=not True
 clock = pygame.time.Clock()
+downarrow=uparrow=leftarrow=rightarrow=False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,59 +46,47 @@ while True:
             rightarrow= event.key==pygame.K_RIGHT
             uparrow= event.key==pygame.K_UP
 
-            if downarrow and snakeHead[1] < w_height:
-                if snakeHead[1]+blockSize in snakeList:
-                    print("Game over")
-                    break
-                coordinate_update(snakeList)
-                #print(snakeList)
-                snakeHead[1]+=blockSize
+    if downarrow:
+        if ([snakeHead[0],snakeHead[1]+blockSize] in snakeList) or snakeHead[1] >= w_height:
+            print("Game over, your score was", len(snakeList)-1)
+            break
+        coordinate_update(snakeList)
+        snakeHead[1]+=blockSize
 
-                #snakeList[0][1] += blockSize
-                #print(snakeList)
+    elif uparrow:
+        if [snakeHead[0],snakeHead[1]-blockSize] in snakeList or snakeHead[1] <= 0:
+            print("Game over, your score was", len(snakeList)-1)
+            break
+        coordinate_update(snakeList)
+        snakeHead[1]-=blockSize
 
-            elif uparrow and snakeHead[1] > 0:
-                if snakeHead[1]-blockSize in snakeList:
-                    print("Game over")
-                    break
-                coordinate_update(snakeList)
-                snakeHead[1]-=blockSize
+    elif leftarrow:
+        if [snakeHead[0]-blockSize,snakeHead[1]] in snakeList or snakeHead[0] <= 0:
+            print("Game over, your score was", len(snakeList)-1)
+            break
+        coordinate_update(snakeList)
+        snakeHead[0]-=blockSize
 
-            elif leftarrow and snakeHead[0] > 0:
-                if snakeHead[0]-blockSize in snakeList:
-                    print("Game over")
-                    break
-                coordinate_update(snakeList)
-                snakeHead[0]-=blockSize
-
-            elif rightarrow and snakeHead[0] < w_width:
-                if snakeHead[0]+blockSize in snakeList:
-                    print("Game over")
-                    break
-                coordinate_update(snakeList)
-                snakeHead[0]+=blockSize
-
+    elif rightarrow:
+        if [snakeHead[0]+blockSize,snakeHead[1]] in snakeList or snakeHead[0] >= w_width:
+            print("Game over, your score was", len(snakeList)-1)
+            break
+        coordinate_update(snakeList)
+        snakeHead[0]+=blockSize
 
     gameDisplay.fill((0,0,0))
     pygame.draw.rect(gameDisplay,red,(init_x,init_y,blockSize,blockSize))
-    #update check when snake head coordinate = init_x,init_y
-    #check =
     new_x = math.floor(random.randint(0,w_width)/10)*10
     new_y = math.floor(random.randint(0,w_height)/10)*10
     while [new_x,new_y] in snakeList:
         new_x = random.randint(0,w_width)
         new_y = random.randint(0,w_height)
-    #snake_init(blockSize,snakeHead)
     snake_run(blockSize,snakeList)
-    #if(abs(init_x-snakeHead[0])+abs(init_y-snakeHead[1])<10):
     if([init_x,init_y]==snakeHead):
-        #check=True
         snakeList.append([init_x,init_y])
         snakeHead=snakeList[0]
         move_food(init_x,init_y,new_x,new_y)
         init_x, init_y = new_x, new_y
-    #coordinate_update(snakeList)
-    #if check:
 
     pygame.display.update()
     clock.tick(FPS)
